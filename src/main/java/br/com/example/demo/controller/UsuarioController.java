9d584a3b98a1885d574b2ee5014de8754bd8ee95
Allan Krueger <klimber.mail@gmail.com>
2018-10-28 16:44:57 -0300

    Criando Endpoint incluirUsuario

--- src/main/java/br/com/example/demo/controller/UsuarioController.java
+++ src/main/java/br/com/example/demo/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.example.demo.model.Usuario;
@@ -16,6 +18,11 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@PostMapping("/usuarios")
+	public void incluirUsuario(@RequestBody Usuario usuario) {
+		usuarioRepository.save(usuario);
+	}
 	
 
 	@GetMapping("/usuarios")
